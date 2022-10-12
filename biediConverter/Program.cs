e2944d154595b531f5f5cf63e1baf609f9f771e2
dreamforceinc <dreamforce@yandex.ru>
2022-10-12 12:07:09 +0300

    Some refactorings.

--- biediConverter/Program.cs
+++ biediConverter/Program.cs
@@ -16,7 +16,6 @@ namespace biediConverter
 		static object[] attrCompany = app.GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
 		static string appCopyright = (attrCopyright[0] as AssemblyCopyrightAttribute).Copyright;
 		static string appCompany = (attrCompany[0] as AssemblyCompanyAttribute).Company;
-		static uint vehiclesCount = 0, markersCount = 0;
 
 
 		static string parseString(string str) => str.Substring(str.IndexOf('=') + 1);
@@ -28,25 +27,10 @@ namespace biediConverter
 			{
 				str = sr.ReadLine();
 				buf = str.Trim().ToLower();
-				if (buf.StartsWith("position="))
-				{
-					veh.Position = str.Substring(str.IndexOf('=') + 1).Trim('"', ';');
-					//veh.Position = veh.Position.Trim('"', ';');
-				}
-				if (buf.StartsWith("type="))
-				{
-					veh.Type = str.Substring(str.IndexOf('=') + 1).Trim(';');
-					//veh.Type = veh.Type.Trim(';');
-				}
-				if (buf.StartsWith("azimut="))
-				{
-					veh.Azimut = str.Substring(str.IndexOf('=') + 1).Trim('"', ';');
-					//veh.Azimut = veh.Azimut.Trim('"', ';');
-				}
-				if (buf.StartsWith("init=\"_this setvectorup"))
-				{
-					veh.Init = true;
-				}
+				if (buf.StartsWith("position=")) veh.Position = parseString(str).Trim('"', ';');
+				if (buf.StartsWith("type=")) veh.Type = parseString(str).Trim(';');
+				if (buf.StartsWith("azimut=")) veh.Azimut = parseString(str).Trim('"', ';');
+				if (buf.StartsWith("init=\"_this setvectorup")) veh.Init = true;
 			}
 		}
 
@@ -57,77 +41,44 @@ namespace biediConverter
 			{
 				str = sr.ReadLine();
 				buf = str.Trim().ToLower();
-				if (buf.StartsWith("position="))
-				{
-					mrk.Position = str.Substring(str.IndexOf('=') + 1).Trim('"', ';');
-				}
-				if (buf.StartsWith("name="))
-				{
-					mrk.Name = str.Substring(str.IndexOf('=') + 1).TrimEnd(';');
-				}
-				if (buf.StartsWith("text="))
-				{
-					mrk.Text = str.Substring(str.IndexOf('=') + 1).TrimEnd(';');
-				}
-				if (buf.StartsWith("type="))
-				{
-					mrk.Type = str.Substring(str.IndexOf('=') + 1).TrimEnd(';');
-				}
-				if (buf.StartsWith("marker_type="))
-				{
-					mrk.MarkerType = str.Substring(str.IndexOf('=') + 1).TrimEnd(';');
-				}
-				if (buf.StartsWith("color="))
-				{
-					mrk.Color = str.Substring(str.IndexOf('=') + 1).TrimEnd(';');
-				}
-				if (buf.StartsWith("fill="))
-				{
-					mrk.Fill = str.Substring(str.IndexOf('=') + 1).TrimEnd(';');
-				}
-				if (buf.StartsWith("a="))
-				{
-					mrk.A = str.Substring(str.IndexOf('=') + 1).Trim('"', ';');
-				}
-				if (buf.StartsWith("b="))
-				{
-					mrk.B = str.Substring(str.IndexOf('=') + 1).Trim('"', ';');
-				}
+				if (buf.StartsWith("position=")) mrk.Position = parseString(str).Trim('"', ';');
+				if (buf.StartsWith("name=")) mrk.Name = parseString(str).TrimEnd(';');
+				if (buf.StartsWith("text=")) mrk.Text = parseString(str).TrimEnd(';');
+				if (buf.StartsWith("type=")) mrk.Type = parseString(str).TrimEnd(';');
+				if (buf.StartsWith("marker_type=")) mrk.MarkerType = parseString(str).TrimEnd(';');
+				if (buf.StartsWith("color=")) mrk.Color = parseString(str).TrimEnd(';');
+				if (buf.StartsWith("fill=")) mrk.Fill = parseString(str).TrimEnd(';');
+				if (buf.StartsWith("a=")) mrk.A = parseString(str).Trim('"', ';');
+				if (buf.StartsWith("b=")) mrk.B = parseString(str).Trim('"', ';');
 			}
 		}
 
 		static int Main(string[] args)
 		{
-			Console.WriteLine($"{appName} v{appVer}\n{appCopyright} by {appCompany}\n");
+			Console.WriteLine($"{appName} v{appVer} ({appFullVer})\n{appCopyright} by {appCompany}");
 
 			if (args.Count() != 1)
 			{
 				Console.WriteLine($"usage: {appName}.exe \"path\\to\\file.biedi\"");
-				Console.WriteLine("Press any key to quit.");
-				Console.ReadKey();
 				return 1;
 			}
-			if (!File.Exists(args[0]))
+
+			string sourcePath = args[0];
+			if (!File.Exists(sourcePath))
 			{
-				Console.WriteLine($"{appName}: file '{args[0]}' does not exist!");
-				Console.WriteLine("Press any key to quit.");
-				Console.ReadKey();
+				Console.WriteLine($"ERROR: file '{sourcePath}' does not exist!");
 				return 2;
 			}
-			string path = Path.GetDirectoryName(args[0]);
-			string filename = Path.GetFileNameWithoutExtension(args[0]);
-			string fileext = Path.GetExtension(args[0]);
+
+			string fileext = Path.GetExtension(sourcePath);
 			if (fileext.ToLower() != ".biedi")
 			{
-				Console.WriteLine($"{appName}: file '{args[0]}' is not .biedi file!");
-				Console.WriteLine("Press any key to quit.");
-				Console.ReadKey();
+				Console.WriteLine($"ERROR: file '{sourcePath}' is not .biedi file!");
 				return 3;
 			}
-			string sqfFile = Path.Combine(path, filename + ".sqf");
-#if DEBUG
-			sqfFile = "C:\\Users\\W0LF\\Documents\\ArmA 2\\missions\\test.Chernarus\\out.sqf";
-#endif
+
+			string sqfFile = Path.Combine(Path.GetDirectoryName(sourcePath), "output.sqf");
+
 			List<vehObject> vehicles = new List<vehObject>();
 			List<markerObject> markers = new List<markerObject>();
 			string bufLine;
@@ -142,14 +93,12 @@ namespace biediConverter
 						if (bufLine.StartsWith("objecttype=\"vehicle\";"))
 						{
 							vehObject item = new vehObject();
-							vehiclesCount++;
 							parseVehicle(sr, bufLine, item);
 							vehicles.Add(item);
 						}
 						if (bufLine.StartsWith("objecttype=\"marker\";"))
 						{
 							markerObject marker = new markerObject();
-							markersCount++;
 							parseMarker(sr, bufLine, marker);
 							markers.Add(marker);
 						}
@@ -161,15 +110,17 @@ namespace biediConverter
 				Console.WriteLine(e.Message);
 			}
 
-			if (vehiclesCount > 0 || markersCount > 0)
+			if (vehicles.Count > 0 || markers.Count > 0)
 			{
 				try
 				{
 					using (StreamWriter sw = new StreamWriter(sqfFile))
 					{
-						sw.WriteLine($"// Created by {appName} v{appVer}\n// {appCopyright} by {appCompany}\n");
-						if (vehiclesCount > 0)
+						sw.WriteLine($"// Created by {appName} v{appVer} ({appFullVer})\n// {appCopyright} by {appCompany}");
+						if (vehicles.Count > 0)
 						{
+							sw.WriteLine();
+							Console.WriteLine();
 							sw.WriteLine("_obj = [");
 							for (int i = 0; i < vehicles.Count(); ++i)
 							{
@@ -179,7 +130,7 @@ namespace biediConverter
 							sw.WriteLine("];");
 							sw.WriteLine("{\n\t_this = (_x select 0) createVehicle (_x select 1);\n\t_this setPos (_x select 1);\n\t_this setDir (_x select 2);\n\tif (_x select 3) then {_this setVectorUp [0,0,1];};\n} forEach _obj;");
 						}
-						if (markersCount > 0)
+						if (markers.Count > 0)
 						{
 							sw.WriteLine();
 							Console.WriteLine();
@@ -196,17 +147,7 @@ namespace biediConverter
 					Console.WriteLine(e.Message);
 				}
 			}
-			else if (vehiclesCount == 0)
-			{
-				Console.WriteLine("\nNo vehicles found.");
-			}
-			else if (markersCount == 0)
-			{
-				Console.WriteLine("\nNo markers found.");
-			}
 
-			Console.WriteLine("Press any key to quit.");
-			Console.ReadKey();
 			return 0;
 		}
 	}
